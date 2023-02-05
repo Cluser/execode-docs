429c95b27382dae594d8e19fa1ff4dca7b29be99
execode-bf <bartlomiej.filipiak@execode.pl>
2023-02-05 02:04:34 +0100

    Save init version

--- server/src/configuration/index.ts
+++ server/src/configuration/index.ts
@@ -3,6 +3,6 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 export const SQLITE_CONFIG: TypeOrmModuleOptions = {
     type: "sqlite",
     database: "database",
-    entities: [process.cwd() + "/**/*.entity{.ts,.js}"],
+    autoLoadEntities: true,
     synchronize: true,
 };
